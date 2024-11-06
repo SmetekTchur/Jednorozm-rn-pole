8b1e1303bd0ee1aa65f6eca38a7b301da0ff9e76
Zmar <159122092+SmetekTchur@users.noreply.github.com>
2024-11-06 10:16:25 +0000

    final_Tchur_

--- jpTchur.py
+++ jpTchur.py
@@ -1,1 +1,16 @@
-
+#základní pole
+array = ["martin","genb","gamba","skibu","mamlas"]
+print(array)
+#přidání pomocí append
+array.append ("vítr")
+print(array)
+#daní pryč pomocí remove
+array.remove ("genb")
+print(array)
+#vyměnění slova v poli
+array [4] = "slunce"
+print(array)
+#rozšíření pomocí extend
+mame = ["dg", "gug"]
+array.extend (mame)
+print(array)
